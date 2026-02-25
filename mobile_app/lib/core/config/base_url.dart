22f543447b16929eaf8fd0e6b728bb32cb767012
Dhafina Bening N.B <dhafinaaa05@gmail.com>
2026-02-25 11:23:38 +0700

    nyambungin database

---
+++ mobile_app/lib/core/config/base_url.dart
@@ -0,0 +1,5 @@
+class AppConfig {
+  // Google Apps Script Web App URL (Versi A)
+  static const String baseUrl =
+      "https://script.google.com/macros/s/AKfycbztRcNUzB2cG4X9z105XF1hJ83_nKKM4LcZNK5lHJ7HVovHWrpIecxDq8uH3uEGGjyz/exec";
+}
