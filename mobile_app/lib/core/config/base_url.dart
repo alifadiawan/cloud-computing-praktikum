8bc98b124da3c9c26e652d71bb56ccd10ad0458a
reynaldisusilowaskito <reeynaldisousiloq2@gmail.com>
2026-04-16 19:49:58 +0700

    ganti /exec base_url

--- mobile_app/lib/core/config/base_url.dart
+++ mobile_app/lib/core/config/base_url.dart
@@ -1,5 +1,5 @@
 class AppConfig {
   // Google Apps Script Web App URL (Versi A)
   static const String baseUrl =
-      "https://script.google.com/macros/s/AKfycbztRcNUzB2cG4X9z105XF1hJ83_nKKM4LcZNK5lHJ7HVovHWrpIecxDq8uH3uEGGjyz/exec";
+      "https://script.google.com/macros/s/AKfycbzXQyqvnr1igO1MDW3E1_9t4w_HMb0BSJ54I-ukFHo1QNo8XCl63NncIQ3OLrD7aLdA/exec";
 }
